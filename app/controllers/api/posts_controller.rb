19453b8a5cc910ec79225d330d5e31e79353b71d
timcev1 <timcev@gmail.com>
2017-11-08 09:26:11 -0500

    added api controller

---
+++ app/controllers/api/posts_controller.rb
@@ -0,0 +1,44 @@
+class Api::PostsController < ApplicationController
+  before_action :post_finder, only: [:show, :edit, :destroy]
+  def index
+    render json: Post.all
+  end
+
+  def create
+    post = Post.new(post_params)
+    if post.save
+      render json: post
+    else
+      render json: { message: post.errors}, status: 400
+    end
+  end
+
+  def show
+    render json: @post
+  end
+
+  def update
+    if @post.update(post_params)
+      render json: @post
+    else
+      render json: {message: @post.errors}, status: 400
+    end
+  end
+
+  def destroy
+    if @post.destroy
+      render status: 204
+    else
+      render json: { message: "unable to remove this post"}, status: 400
+    end
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
+    def post_finder
+      @post = Post.find(params[:id])
+    end
+end
